386e39e3194c24c899dd3ec1182b270a38b0e3f0
BLVCK1100 <108452308+BLVCK1100@users.noreply.github.com>
2022-07-07 09:27:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -34,8 +34,12 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true') ] ,
+          [
+            InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about_menu'),
+            InlineKeyboardButton('ᴄʟᴏsᴇ', callback_data='close')
         ]]
+              
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
